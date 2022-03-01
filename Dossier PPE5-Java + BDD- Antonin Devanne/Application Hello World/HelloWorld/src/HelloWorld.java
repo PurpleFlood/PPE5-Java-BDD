8e028bb381da9ec6b1c4615757cba7b351305e9c
purple_flood <49845374+PurpleFlood@users.noreply.github.com>
2022-03-01 10:32:56 +0100

    Add files via upload

---
+++ Dossier PPE5-Java + BDD- Antonin Devanne/Application Hello World/HelloWorld/src/HelloWorld.java
@@ -0,0 +1,9 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		int n = 5;
+		System.out.println(n);
+	}
+
+}
---
+++ Dossier PPE5-Java + BDD- Antonin Devanne/Application Hello World/HelloWorld/src/Lecture.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Lecture {
+	
+	static Scanner clavier = new Scanner(System.in);
+	public static void main(String[] args) {
+		int n = 0;
+		String str1 = "";
+		String str2 = "";
+		
+		System.out.print("Entrez un entier : ");
+		n = clavier.nextInt(); 
+		clavier.nextLine();
+		
+		System.out.print("Entrez du texte : ");
+		str1 = clavier.nextLine(); 
+		
+		System.out.print("Entrez du texte : ");
+		str2 = clavier.nextLine(); 
+		
+		System.out.println("n = " + n);
+		System.out.println("str1 = " + str1);
+		System.out.println("str2 = " + str2);
+		
+	}
+}
